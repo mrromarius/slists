e2b33b648f16f0ddad2fade89e1d12f45855c6e6
MrRomarius <Romashev.Al.S@gmail.com>
2022-04-14 16:49:52 +0500

    фт теперь проверфет возможность ввести элемент списка

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -20,18 +22,33 @@ class NewVisitorTest(unittest.TestCase):
 
         # Она видит, что заголовок и шапка страницы говорят о списках неотложных дел
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Закончить тест!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # ей сразу же предлагается ввести элемент списка
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'), 
+            'Введите элемент списка'
+        )
 
         # она выбирает в текстовом поле "купить павлиньи перья" (ее хобби - вязание рыболовных мушек)
+        inputbox.send_keys('Купить павлиньи перья')
 
-        # когда она нажимает ентер, страница обновляется, и теперь страница содержит "1. купить павлиньи перья" в качестве элемента списка
+        # когда она нажимает ентер, страница обновляется, и теперь страница содержит 
+        # "1. купить павлиньи перья" в качестве элемента списка
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1. Купить павлиньи перья' for row in rows)
+        )
         # текстовое поле по-прежнему приглашает ее добавить еще один элемент.
         # она заводит "Сделать мушку из павлиньих перьев"
         # (Эдит очень методичка)
-
+        self.fail('Закончить тест!')
         # страница сново обновляется, и теперь показывает оба элемента ее списка
 
         # Эдит интересно, запомнит ли сайт ее спсико. Далее она видит, что
