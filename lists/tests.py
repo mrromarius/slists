880df6707ece6c48b27c14cd58052f0cdc8bd37c
Alexander Romashev <romashev.al.s@gmail.com>
2022-04-22 14:56:18 +0500

    Переадресация после ПОСТ запроса и фиксация элементов в списке

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,5 @@
+from pydoc import doc
+from telnetlib import DO
 from urllib import response
 from lists.models import Item
 from django.http import HttpRequest
@@ -17,15 +19,41 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         '''тест:можно сохранить пост запрос'''
-        response = self.client.post('/', data={'item_text':'Новый элемент списка'})
-        self.assertIn('Новый элемент списка', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
-        
+        response = self.client.post(
+            '/', data={'item_text': 'Новый элемент списка'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'Новый элемент списка')
+
+    def test_redirects_after_POST(self):
+        '''тест проверяет переадресацию после пост запроса'''
+        response = self.client.post(
+            '/', data={'item_text': 'Новый элемент списка'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_saves_items_when_nessesary(self):
+        '''тест на сохранение элементов, только когда нужно'''
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self):
+        '''тест: отоброжаются все элементы в списке'''
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
+
 class ItemModelTest(TestCase):
     '''тест модели элемента списка'''
 
     def test_saving_and_retrieving_items(self):
         '''тест сохранения и получение элементов списка'''
+        # FIXME: тет слишком длинный
         first_item = Item()
         first_item.text = 'Первый элемент списка'
         first_item.save()
