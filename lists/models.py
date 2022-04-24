97befd8efb8b48292afe866f530297d2bc9e3f11
Alexander Romashev <romashev.al.s@gmail.com>
2022-04-24 22:49:16 +0500

    Новая модель листов

--- lists/models.py
+++ lists/models.py
@@ -1,5 +1,11 @@
+from tkinter import CASCADE
 from django.db import models
+
+class List(models.Model):
+    '''списки'''
+    pass
 
 class Item(models.Model):
     ''''элемент списка'''
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
--- lists/tests.py
+++ lists/tests.py
@@ -60,8 +60,9 @@ class ListViewTest(TestCase):
 
     def test_displays_all_items(self):
         '''тест: отоброжаются все элементы в списке'''
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/uniqurl/')
 
