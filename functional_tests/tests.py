af75325edb90041ae8191a84323668f1aa08cbaf
Alexander Romashev <romashev.al.s@gmail.com>
2022-04-25 23:10:12 +0500

    Первые шаги фт для макетирования + стилевого оформления

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -38,6 +38,33 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
+    def test_layout_and_styling(self):
+        '''тест макета стилевого оформления'''
+        # Эдит открывает домашнюю страницу
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Она замечает что поле ввода аккуратно отцетровано
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=10
+            )
+        
+        # Она начинает новый список и видит что там поле тоже центрировано
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_tabel('1. testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=10
+        )
+
+
+
     def test_can_start_a_list_for_one_user(self):
         # Эдит слышала про крутое новое онлайн-приложение со списком
         # неотложных дел. она решает оценить его домашнюю страницу
