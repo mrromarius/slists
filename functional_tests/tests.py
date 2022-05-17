801faf0d79797f654d1e6a972cfd7d7c0ff0b844
MrRomarius <Romashev.Al.S@gmail.com>
2022-05-17 16:30:30 +0500

    Настроен исполнитель ФТ для тестирования промежуточного сервера

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,6 +3,7 @@ import sys
 import time
 import unittest
 from sys import platform
+import os
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -19,6 +20,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         '''установка'''
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
 
     def tearDown(self):
         '''уничтожение'''
