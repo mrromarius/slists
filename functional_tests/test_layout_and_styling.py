bfcd3b5264959cddb6dffa7071ebf3d7f946141a
MrRomarius <Romashev.Al.S@gmail.com>
2022-05-23 12:42:27 +0500

    Перемещены ФТ в свои собственные отдельные файлы

---
+++ functional_tests/test_layout_and_styling.py
@@ -0,0 +1,32 @@
+from .base import FunctionalTest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+MAX_WAIT = 10
+
+class LayoutAndStylingTest(FunctionalTest):
+    '''тест макета и стилевого оформления'''
+    def test_layout_and_styling(self):
+        '''тест макета стилевого оформления'''
+        # Эдит открывает домашнюю страницу
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Она замечает что поле ввода аккуратно отцетровано
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=10
+            )
+        
+        # Она начинает новый список и видит что там поле тоже центрировано
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_tabel('1. testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=10
+        )
