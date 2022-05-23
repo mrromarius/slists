aba8a9fcd41ee8d69096f47b78c0b93c534cf1a3
MrRomarius <Romashev.Al.S@gmail.com>
2022-05-23 14:36:22 +0500

    Описане новый ФТ по валидации данных

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -12,17 +12,30 @@ class ItemValidationTest(FunctionalTest):
         '''тест: нельзя добавлять пустые элементы списка'''
         # Эдит открывает домашнюю страницу и случаной пытается отправить 
         # постой элемент списка. Она нажимает Энтер на пустом поле воода
-
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
         # Домашняя страница обновляется и появлется сообщение об ошибке
         # которое говорит, что элемент списка не должен быть пустым
-
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "Поле не должно быть пустым"
+        )) 
         # Она пробует снова, теперь с неким текстом для элемента, и теперь это работает
-
+        self.browser.find_element_by_id('id_new_item').send_keys('Купить молока')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_tabel('1. Купить молока')
         # как ни странно, Эдит решает отправить второй пустой элемент списка
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # Она получается аналогичное предупреждение на странице списка
-
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "Поле не должно быть пустым"
+        ))
         # Она получает аналогичное предупреждение на странице списка
-
+        self.browser.find_element_by_id('id_new_item').send_keys('Получить чаёк')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_tabel('1. Купить молока')
+        self.wait_for_row_in_list_tabel('2. Получить чаёк')
         #  И она может его исправить, заполнив поле неким текстом
-        self.fail('напиши меня!')
+        
