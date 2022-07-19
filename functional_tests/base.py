c9aff5090a730b8548e669e1848170b5f452cd3f
Alexander Romashev <romashev.al.s@gmail.com>
2022-07-19 21:24:08 +0500

    Сделаны обертки на wait. Гл20ст409

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,3 +1,4 @@
+from multiprocessing.connection import wait
 import time
 import os
 
@@ -8,6 +9,7 @@ from selenium.common.exceptions import WebDriverException
 
 
 MAX_WAIT = 10
+
 
 class FunctionalTest(StaticLiveServerTestCase):
     '''функциональный тест'''
@@ -24,48 +26,44 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.browser.refresh()
         self.browser.quit()
 
-    def wait_for_row_in_list_tabel(self, row_text):
-        '''подтверждение строки в таблице списка'''
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements(By.TAG_NAME, 'tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except(AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
+    def wait(fn):
+        def modified_fn(*args, **kwargs):
+            start_time = time.time()
+            while True:
+                try:
+                    return fn(*args, **kwargs)
+                except (AssertionError, WebDriverException) as e:
+                    if time.time() - start_time > MAX_WAIT:
+                        raise e
+                    time.sleep(0.5)
+        return modified_fn
 
-    
+    @wait
+    def wait_for_row_in_list_tabel(self, row_text):
+        '''ожидание строки в таблице списка'''
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+    @wait
     def wait_for(self, fn):
         '''ожидание'''
-        start_time = time.time()
-        while True:
-            try:
-                return fn()
-            except(AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
+        return fn()
 
     def get_item_input_box(self):
         '''получить поле ввода для элемента'''
         return self.browser.find_element_by_id('id_text')
 
+    @wait
     def wait_to_be_logged_in(self, email):
         '''ожидать входа в систему'''
-        self.wait_for(
-            lambda:self.browser.find_element_by_link_text('Выйти')
-        )
+        self.browser.find_element_by_link_text('Выйти')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn(email, navbar.text)
 
+    @wait
     def wait_to_be_logged_out(self, email):
         '''ожидать выходать из системы'''
-        self.wait_for(
-            lambda:self.browser.find_element_by_name('email')
-        )
+        self.browser.find_element_by_name('email')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertNotIn(email, navbar.text)
